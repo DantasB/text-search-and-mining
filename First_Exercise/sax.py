4bdeb0b06e5d0159df803939c05a676099b04d7e
Bruno Dantas <bruno.d.paiva@hotmail.com>
2022-04-09 18:16:54 -0300

    Finish sax parser implementation

--- First_Exercise/sax.py
+++ First_Exercise/sax.py
@@ -1,27 +1,37 @@
-import xml.sax
+from xml.sax import make_parser, ContentHandler
+
+INPUT_PATH = 'Data/cf79.xml'
+OUTPUT_PATH = 'Outputs/titulo.xml'
+
 
 class TitleHandler(ContentHandler):
-    INPUT_PATH = 'First_Exercise/Data/cf79.xml'
-    OUTPUT_PATH = 'First_Exercise/Outputs/autores.xml'
+    """ Class created to extract the title tag from a given xml
+    """
 
     def __init__(self):
         self.current_data = ""
         self.title = ""
-        
-    def startElement(self, tag, attributes):
-        if tag == "TITLE":
-            current_data = tag
 
-    def endElement(self, tag):
-        if self.currentTag == "TITLE":
+    def startElement(self, tag, _):
+        if tag == "TITLE":
+            self.current_data = tag
+
+    def endElement(self, _):
+        if self.current_data == "TITLE":
             output_file.write(f'<TITLE>{self.title}</TITLE>\n')
 
+        self.current_data = ""
+        self.title = ""
+
+    def characters(self, content):
+        if self.current_data == "TITLE":
+            self.title += content
+
+
 if __name__ == "__main__":
-    # create an XMLReader
-    parser = xml.sax.make_parser()
-    # turn off namepsaces
-    parser.setFeature(xml.sax.handler.feature_namespaces, 0)
-    # override the default ContextHandler
-    Handler=TitleHandler()
+    parser = make_parser()
+    Handler = TitleHandler()
+
     parser.setContentHandler(Handler)
-    parser.parse(Handler.INPUT_PATH)
+    with open(OUTPUT_PATH, 'w') as output_file:
+        parser.parse(INPUT_PATH)
